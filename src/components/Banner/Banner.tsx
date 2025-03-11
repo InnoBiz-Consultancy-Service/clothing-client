41488028607fbc48b3c7489edba581ecc3edae8d
Jeem312 <shanjidajeem312@gmail.com>
2025-03-12 00:27:20 +0600

    re-designed banner page

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -56,7 +56,7 @@ const Banner = () => {
       </Swiper>
 
       {/* T-Shirt Slider with Dynamic Text */}
-      <div className="absolute bottom-5 left-1/2 transform -translate-x-1/2 w-[90%] md:w-[60%] z-10">
+      <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 w-[90%] md:w-[60%] z-10">
         <Swiper
           modules={[Autoplay]}
           autoplay={{ delay: 3000, disableOnInteraction: false }}
