8819ffc9b7177c22ccf3aeb809ca3b1e7df6e933
arifadnan05 <arifahamedadnan@gmail.com>
2025-03-23 21:30:44 +0600

    build (release): downgrade next.js version 15 to 14

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,19 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
 import Footer from "@/components/Shared/Footer/Footer";
 import Navbar from "@/components/Shared/Navbar/Navbar";
 import TopNav from "@/components/Shared/Navbar/TopNav/TopNav";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -28,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`antialiased`}
       >
         <TopNav />
         <div className="sticky top-0 w-full z-50">
